023314262182b3e458f9210f5fdde2689d06a423
Pawnpug <popcorn.wennberg@gmail.com>
2025-04-21 14:45:39 +0200

    metoden exportRecordings skapad

--- Exercise3.java
+++ Exercise3.java
@@ -27,34 +27,35 @@ public class Exercise3 {
 		fileWriter.close();
 	}
 
-	public void importRecordings(String fileName) throws IOException {
+	public void importRecordings(String fileName) {
 
-	FileReader fileReader = new FileReader(fileName);
-	BufferedReader reader = new BufferedReader(fileReader); // Osäker på hur BufferedReader funkar, snodde dessa två rader från genomgången av lektionen
+	try (FileReader fileReader = new FileReader(fileName);
+	BufferedReader reader = new BufferedReader(fileReader)) {// Osäker på hur BufferedReader funkar, snodde dessa två rader från genomgången av lektionen
 
-	String line = reader.readLine(); // Läser in första siffran som ska representera antalet skivor som en sträng då
-	int totalRecordings = Integer.parseInt(line.trim()); // Förvandlar strängen till en Int
-			for( int i = 0; i < totalRecordings;i++) { // Kör en foor loop för hela filen ex 6 recs kör vi loopen 6 gånger
-				line = reader.readLine(); // läser vi andra rader i dokumentet
-				String[] parts = line.split(";"); // delar upp inlästa raden i delar, separerade med semicolon
-				String artist = parts[0];
-				String title = parts[1];
-				int year = Integer.parseInt(parts[2]);
+		String line = reader.readLine(); // Läser in första siffran som ska representera antalet skivor som en sträng då
+		int totalRecordings = Integer.parseInt(line.trim()); // Förvandlar strängen till en Int
+		for (int i = 0; i < totalRecordings; i++) { // Kör en foor loop för hela filen ex 6 recs kör vi loopen 6 gånger
+			line = reader.readLine(); // läser vi andra rader i dokumentet
+			String[] parts = line.split(";"); // delar upp inlästa raden i delar, separerade med semicolon
+			String artist = parts[0];
+			String title = parts[1];
+			int year = Integer.parseInt(parts[2]);
 
-				Set<String> genres = new HashSet<>(); // Spara genres tillfälligt
-				line = reader.readLine(); // Hämta antalet genres i sträng format
-				int numberOfGenres = Integer.parseInt(line); // Sträng till int format
-				for(int j = 0; j < numberOfGenres; j++) { // Iterera över antalet genres och lägg in i Set
-					line = reader.readLine();
-					genres.add(line);
-				}
+			Set<String> genres = new HashSet<>(); // Spara genres tillfälligt
+			line = reader.readLine(); // Hämta antalet genres i sträng format
+			int numberOfGenres = Integer.parseInt(line); // Sträng till int format
+			for (int j = 0; j < numberOfGenres; j++) { // Iterera över antalet genres och lägg in i Set
+				line = reader.readLine();
+				genres.add(line);
+			}
 
-				Recording recording = new Recording(artist, title, year, genres); // Skapar ett nytt Recroding objekt
-				recordings.add(recording); // Sparar nya objektet i recordings listan
+			Recording recording = new Recording(artist, title, year, genres); // Skapar ett nytt Recroding objekt
+			recordings.add(recording); // Sparar nya objektet i recordings listan
 
+		}
+	} catch (IOException e){
+		e.printStackTrace();
 	}
-			fileReader.close();
-			reader.close();
 }
 
 
